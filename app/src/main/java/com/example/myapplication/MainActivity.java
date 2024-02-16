44ab5d054e4f71d80ba4b2aebf7d0770dc6da092
Kevin Cedeño <joelcedeno612@gmail.com>
2024-02-15 23:31:25 -0500

    Actualización

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,140 +1,106 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
-
-import android.graphics.Color;
-import android.graphics.Point;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.PolylineOptions;
 
-import java.text.DecimalFormat;
-import java.util.ArrayList;
+import java.util.HashMap;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
-    GoogleMap mapa;
-
-    int contador;
-    ArrayList<LatLng> puntos;
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap mapa;
+    private HashMap<String, String[]> infoMarkers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                        .findFragmentById(R.id.map);
+
+        infoMarkers = new HashMap<>();
+        infoMarkers.put("Facultad de Ciencias de la Salud", new String[]{"Formar íntegramente profesionales de la salud con sólidas bases científicas, humanísticas que le permitan " +
+                "actuar ante las necesidades de salud brindando una atención integral demostrando alto compromiso ético en el cuidado de la vida, contribuyendo con su " +
+                "conocimiento al desarrollo de la investigación, ciencia e innovación mejorando las condiciones de bienestar de la comunidad garantizando el derecho a " +
+                "la salud.", String.valueOf(R.drawable.enfermeria)});
+        infoMarkers.put("Facultad de Ciencias Empresariales", new String[]{"La Facultad de Ciencias Empresariales de la UTEQ será líder en el campo académico, de investigación, " +
+                "extensión y gestión de los niveles Técnicos, Tecnológicos, Pregrado y Postgrado, que permite incorporar Recursos Humanos al medio productivo científico, " +
+                "técnico y social de manera eficaz y de acuerdo a las exigencias de una sociedad competitiva.", String.valueOf(R.drawable.empresariales)});
+        infoMarkers.put("Facultad de Ciencias Sociales, Económicas y Financieras", new String[]{"La Facultad de Ciencias Sociales Económicas y Financieras forma profesionales " +
+                "comprometidos con la comunidad, con suficientes bases teóricas y prácticas sobre el mejor uso de los recursos planteando soluciones a los problemas que impiden " +
+                "alcanzar el bienestar económico y financiero de toda la sociedad.", String.valueOf(R.drawable.sociales)});
+        infoMarkers.put("Facultad de Ciencias de la Educación", new String[]{"Luego de un año de creación la Facultad de Ciencias de la Educación (FCEdu) cuenta con más de 500 " +
+                "estudiantes.", String.valueOf(R.drawable.educacion)});
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        contador = 0;
-        puntos = new ArrayList<LatLng>();
     }
+
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(@NonNull GoogleMap googleMap) {
         mapa = googleMap;
         mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         mapa.getUiSettings().setZoomControlsEnabled(true);
 
-       // LatLng madrid = new LatLng(-1.0122163925312702, -79.46962177336476);
-      //  CameraPosition camPos = new CameraPosition.Builder()
-        //        .target(madrid)
-          //      .zoom(25)
-            //    .bearing(45) //noreste arriba
-              //  .tilt(70) //punto de vista de la cámara 70 grados
-                //.build();
-      //  CameraUpdate camUpd3 =
-        //        CameraUpdateFactory.newCameraPosition(camPos);
-        //mapa.animateCamera(camUpd3);
+        LatLng facultad1 = new LatLng(-1.012859, -79.469363);
+        LatLng facultad2 = new LatLng(-1.012269, -79.470227);
+        LatLng facultad3 = new LatLng(-1.012590, -79.470522);
+        LatLng facultad4 = new LatLng(-1.012569, -79.471015);
 
-        CameraUpdate camUpd1 = CameraUpdateFactory
-                .newLatLngZoom(new LatLng(-1.0122, -79.4696), 20);
-        mapa.moveCamera(camUpd1);
-        mapa.setOnMapClickListener(this);
+        agregarMarcador(facultad1, "Facultad de Ciencias de la Salud", BitmapDescriptorFactory.HUE_RED);
+        agregarMarcador(facultad2, "Facultad de Ciencias Empresariales", BitmapDescriptorFactory.HUE_GREEN);
+        agregarMarcador(facultad3, "Facultad de Ciencias Sociales, Económicas y Financieras", BitmapDescriptorFactory.HUE_BLUE);
+        agregarMarcador(facultad4, "Facultad de Ciencias de la Educación", BitmapDescriptorFactory.HUE_YELLOW);
 
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(facultad1, 18);
+        mapa.animateCamera(cameraUpdate);
+
+        mapa.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+                View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.informacion, null);
+                TextView titleTextView = view.findViewById(R.id.facul);
+                TextView infoTextView = view.findViewById(R.id.informacion);
+                ImageView imageView = view.findViewById(R.id.ImgFacultad);
+
+                String[] info = infoMarkers.get(marker.getTitle());
+                if (info != null) {
+                    titleTextView.setText(marker.getTitle());
+                    infoTextView.setText(info[0]);
+                    imageView.setImageResource(Integer.parseInt(info[1]));
+                }
+
+                return view;
+            }
+        });
     }
 
-    @Override
-    public void onMapClick(LatLng latLng) {
-
-        /*Projection proj = mapa.getProjection();
-        Point coord = proj.toScreenLocation(latLng);*/
-
-        TextView lblLat = findViewById(R.id.lblLatitud);
-        lblLat.setText(String.format("%.4f",latLng.latitude));
-
-        TextView lblLong = findViewById(R.id.lblLongotid);
-        lblLong.setText(String.format("%.4f",latLng.longitude));
-
-        mapa.addMarker(new MarkerOptions().position(latLng)
-                .title("Marcador"));
-
-        contador = contador + 1;
-        puntos.add(latLng);
-        if(contador == 4){
-            PolylineOptions lineas = new PolylineOptions()
-                    .add(puntos.get(0))
-                    .add(puntos.get(1))
-                    .add(puntos.get(2))
-                    .add(puntos.get(3))
-                    .add(puntos.get(0));
-            lineas.width(8);
-            lineas.color(Color.RED);
-            mapa.addPolyline(lineas);
-            contador = 0;
-            puntos.clear();
-        }
-
-        /*
-        TextView lblLat = findViewById(R.id.lblLatitud);
-        TextView lblLong = findViewById(R.id.lblLongotid);
-
-        // Formatear latitud y longitud a 4 decimales
-        DecimalFormat decimalFormat = new DecimalFormat("#.####");
-        String formattedLat = decimalFormat.format(latLng.latitude);
-        String formattedLong = decimalFormat.format(latLng.longitude);
-
-        lblLat.setText(formattedLat);
-        lblLong.setText(formattedLong);*/
-
-
-        /*Toast.makeText(
-                MainActivity.this,
-                "Click\n" +
-                        "Lat: " + latLng.latitude + "\n" +
-                        "Lng: " + latLng.longitude + "\n" +
-                        "X: " + coord.x + " - Y: " + coord.y,
-                Toast.LENGTH_SHORT).show();*/
-    }
-    public void PintarRectUteq(View view){
-        mapa.addMarker(new MarkerOptions().position(
-                new LatLng(-1.0134, -79.4672)));
-        mapa.addMarker(new MarkerOptions().position(
-                new LatLng(-1.0123, -79.4671)));
-        mapa.addMarker(new MarkerOptions().position(
-                new LatLng(-1.0120, -79.4714)));
-        mapa.addMarker(new MarkerOptions().position(
-                new LatLng(-1.0131, -79.4716)));
-
-        PolylineOptions lineas = new
-                PolylineOptions()
-                .add(new LatLng(-1.0134, -79.4672))
-                .add(new LatLng(-1.0123, -79.4671))
-                .add(new LatLng(-1.0120, -79.4714))
-                .add(new LatLng(-1.0131, -79.4716))
-                .add(new LatLng(-1.0134, -79.4672));
-        lineas.width(8);
-        lineas.color(Color.RED);
-        mapa.addPolyline(lineas);
+    private void agregarMarcador(LatLng posicion, String titulo, float color) {
+        mapa.addMarker(new MarkerOptions()
+                .position(posicion)
+                .title(titulo)
+                .icon(BitmapDescriptorFactory.defaultMarker(color)));
     }
 }
+
+
+
+
+
+
+
